1afdde4dd0b97eb29dce0d0cd18c8058a9cf8922
Mateus Bessa <mateusbessacontato@gmail.com>
2024-02-23 15:45:34 -0300

    Palindrome

---
+++ Palindrome/palindrome.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, result, q, remainder;
+    printf("Please enter the number: \n");
+    scanf("%d", &n);
+
+    q = n;
+
+    while (q != 0)
+    {
+        remainder = q % 10;
+        result = result*10 + remainder;
+        q = q/10;
+    }
+
+    if (result == n)
+    {
+        printf("The number is a palindrome!\n");
+    }
+    else
+    {
+        printf("The number is not a palindrome!\n");
+    }
+    return 0;
+}
--- Tabuada/tabuada.c
+++ Tabuada/tabuada.c
@@ -6,13 +6,11 @@ int main()
 
     for (i = 1; i <= 10; i++)
     {
-        i == 1 ? printf("-----------------------------------------------\n") : 0;
-        printf("Tabuada do %d\n\n", i);
         for (j = 1; j <= 10; j++)
         {
             result = i * j;
             printf("%d x %d = %d\n", i, j, result);
         }
-        printf("-----------------------------------------------\n");
+        printf("\n");
     }
 }
